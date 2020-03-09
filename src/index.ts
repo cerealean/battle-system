14d932869d5b2621ab0a64e827c39dff8fc330d5
Michael Crawford <michael.crawford@veteransunited.com>
2020-03-09 13:23:28 -0500

    Added more to package.json and removed index.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Entity } from "./entity";
-import { BattleEntity } from "./battle-entity";
-import { BattleOrchestrator } from "./battle-orchestrator";
-
-module.exports = [
-    Entity,
-    BattleEntity,
-    BattleOrchestrator
-];
